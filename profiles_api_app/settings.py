f5c4099c48c7071821a7d0493a42bde12208cd9d
adityaashivam@gmail.com <adityaashivam@gmail.com>
2024-07-19 16:04:18 +0530

    Added custom user model

--- profiles_api_app/settings.py
+++ profiles_api_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profile_api',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'profile_api.UserProfile'
+
